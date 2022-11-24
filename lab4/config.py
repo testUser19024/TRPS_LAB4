8750bb7415ae80d397bd85a5e9fd5ba475aadab6
testUser19024 <119002197+testUser19024@users.noreply.github.com>
2022-11-24 19:08:57 +0200

    added files

---
+++ lab4/config.py
@@ -0,0 +1,5 @@
+input_filepath = 'C:/Users/Lenovo/Documents/University/4 year - 1 tearm/ТРПС/TRPS_LAB4/lab4_data/input.txt'
+grid_size = 10
+max_countries_amount = 20
+initial_city_balance = 1_000_000
+representative_portion = 1_000
